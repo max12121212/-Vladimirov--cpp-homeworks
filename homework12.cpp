4cbe2cedeae1af1d2f7b700df525a3fe568b0ef4
Vladimirovcpphomeworks <maksimvlad19@gmail.com>
2026-01-06 23:28:20 +0500

    First commit

--- homework12.cpp
+++ homework12.cpp
@@ -1,7 +1,20 @@
+// Программа: Персональное приветствие пользователя 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << endl;
+int main()
+{
+    // Задаём переменную для хранения имени
+    string Name;
+
+    // Программа запрашивет имя
+    cout << "Введите ваше имя: ";
+    getline(cin, Name);
+
+    // Вывод персонального приветствия
+    cout << "Здравствуйте " << Name << "! Как ваши дела?" << endl;
+
     return 0;
 }
